95de2623cdc65856921e79d643c9b016f2453dc7
“Alessio <alessiomarcelli04@gmail.com>
2024-03-05 12:53:58 +0100

    ora si

--- src/test/java/it/itsincom/webdev/AppTest.java
+++ src/test/java/it/itsincom/webdev/AppTest.java
@@ -4,15 +4,63 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
+
 /**
  * Unit test for simple App.
  */
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class AppTest {
     /**
      * Rigorous Test.
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void parsedoubleShouldNotWorkOnWordAfterDot() {
+        try {
+            Double value = App.parseDouble("123.a");
+            Assertions.fail(
+                    "Parsedouble non può tornare un valore in caso di stringa con caratteri non numerici dopo il punto");
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+
+        }
+
     }
+
+    public void parsedoubleShouldNotWorkOnWordBeforDot() {
+        try {
+            Double value = App.parseDouble("a.123");
+            Assertions.fail(
+                    "Parsedouble non può tornare un valore in caso di stringa con caratteri non numerici prima il punto");
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+
+        }
+
+    }
+
+    public void parsedoubleShouldNotWorkIfThereIsNothingAfterDot() {
+        try {
+            Double value = App.parseDouble("123.");
+            Assertions.fail("Parsedouble non può tornare un valore in caso di stringa senza caratteri dopo il punto");
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+
+        }
+
+    }
+
+    public void parsedoubleShouldNotWorkIfThereIsNothingBeforDot() {
+        try {
+            Double value = App.parseDouble("123.");
+            Assertions.fail("Parsedouble non può tornare un valore in caso di stringa senza caratteri prima il punto");
+        } catch (IllegalArgumentException e) {
+            // TODO: handle exception
+
+        }
+
+    }
+
 }
